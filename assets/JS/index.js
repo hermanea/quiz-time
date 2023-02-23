9335048a63989dd697d02f559692a6ab4934a240
Herman <evanaherman@gmail.com>
2023-02-22 20:43:03 -0800

    updated quiz questions and edited html

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,54 +1,79 @@
 // Defining the quiz questions.
 let quizQuestions = [
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Rum, Lime, and Mexican Coke?",
+    options: ["Daquiri", "Moscow Mule", "Cuba Libre", "Caipirinha"],
+    answer: "Cuba Libre"
   },
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Gin, Campari, and Sweet Vermouth?",
+    options: ["Alaska", "Negroni", "Boulevardier", "Sidecar"],
+    answer: "Negroni"
   },
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Gin, Yellow Chartreuse, and Orange Bitters?",
+    options: ["Manhattan", "Rob Roy", "Japanese", "Alaska"],
+    answer: "Alaska"
   },
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Cognac, Cointreau, and Lemon Juice?",
+    options: ["Sidecar", "Margarita", "Negroni", "Rob Roy"],
+    answer: "Sidecar"
   },
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Cachaça, Sugar, and Lime?",
+    options: ["Daquiri", "Grasshopper", "Cuba Libre", "Caipirinha"],
+    answer: "Caipirinha"
   },
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Vodka, Kahlúa, and Heavy Cream?",
+    options: ["Grasshopper", "White Russian", "Old-Fashioned", "Moscow Mule"],
+    answer: "White Russian"
   },
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Campari, Sweet Vermouth, and Bourbon?",
+    options: ["Boulevardier", "Alaska", "Manhattan", "Japanese"],
+    answer: "Boulevardier"
   },
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Tequila, Lime Juice, and Cointreau?",
+    options: ["Daquiri", "Moscow Mule", "Margarita", "Sidecar"],
+    answer: "Margarita"
   },
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Vodka, Lime Juice, and Ginger Beer",
+    options: ["White Russian", "Rob Roy", "Japanese", "Moscow Mule"],
+    answer: "Moscow Mule"
   },
   {
-    question: "",
-    options: ["", "", "", ""],
-    answer: ""
+    question: "Which cocktail is created with Scotch, Sweet Vermouth, and Angostura Bitters?",
+    options: ["Rob Roy", "Old-Fashioned", "Negroni", "Alaska"],
+    answer: "Rob Roy"
+  },
+  {
+    question: "Which cocktail is created with Crème de Menthe, Crème de Cacao, and Heavy Cream?",
+    options: ["Grasshopper", "Caipirinha", "White Russian", "Rob Roy"],
+    answer: "Grasshopper"
+  },
+  {
+    question: "Which cocktail is created with Rye Whiskey, Sweet Vermouth, and Angostura Bitters?",
+    options: ["Boulevardier", "Manhattan", "Old-Fashioned", "Sidecar"],
+    answer: "Manhattan"
+  },
+  {
+    question: "Which cocktail is created with Cognac, Orgeat, and Angostura Bitters?",
+    options: ["Alaska", "Daquiri", "Japanese", "Sidecar"],
+    answer: "Japanese"
+  },
+  {
+    question: "Which cocktail is created with White Rum, Lime Juice, and Simple Syrup?",
+    options: ["Margarita", "Cuba Libre", "Caipirinha", "Daquiri"],
+    answer: "Daquiri"
+  },
+  {
+    question: "Which cocktail is created with Bourbon, Sugar, and Angostura Bitters?",
+    options: ["Old-Fashioned", "Manhattan", "Alaska", "Negroni"],
+    answer: "Old-Fashioned"
   }
 ];
 
